86a1633c42ea73dd84b0f08f2de1ee5e252fa86e
Lawrence Osher <lao2205@gmail.com>
2025-04-09 16:19:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/upnext"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to UpNext Development!')})
-app.listen(4000);
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "upnext",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+app.listen(process.env.PORT || 4000);
