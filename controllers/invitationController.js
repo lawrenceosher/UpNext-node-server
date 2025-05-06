1777b26930275e9b2948e2403e2ff24d26235ec8
Lawrence Osher <lao2205@gmail.com>
2025-05-06 19:19:52 -0400

    Added comments to invitation controller

--- controllers/invitationController.js
+++ controllers/invitationController.js
@@ -14,7 +14,42 @@ import { addUserToGroup } from "../services/internal/groupService.js";
 import { addUserToAllGroupQueues } from "../services/internal/queueService.js";
 
 export default function InvitationController(app) {
+  /**
+   * Validates the invitation request to make sure it has valid groupId, invitedBy, and invitedUser.
+   * @param req - The request object
+   * @returns Returns true if the request is valid, false otherwise
+   */
+  const isInvitationValid = (req) =>
+    req.body !== undefined &&
+    req.body.groupId !== undefined &&
+    req.body.groupId !== "" &&
+    req.body.invitedBy !== undefined &&
+    req.body.invitedBy !== "" &&
+    req.body.invitedUser !== undefined &&
+    req.body.invitedUser !== "";
+
+  /**
+   * Validates the status to ensure it is one of the allowed values.
+   * @param status - The status to validate
+   * @returns Returns true if the status is valid, false otherwise
+   */
+  const isStatusValid = (status) =>
+    status !== undefined && (status === "accepted" || status === "declined");
+
+  /**
+   * Sends a new invitation to a user to join a group.
+   * @param req - The request object containing groupId, invitedBy, and invitedUser
+   * @param res - The response object
+   * @returns Either 200 (success) with a JSON response containing the new invitation or 500 (error) with an error message
+   */
   const sendNewInvitation = async (req, res) => {
+    // Validate the request
+    if (!isInvitationValid(req)) {
+      res.status(400).json("Invalid invitation");
+      return;
+    }
+
+    // Extract the groupId, invitedBy, and invitedUser from the request body
     const { groupId, invitedBy, invitedUser } = req.body;
 
     try {
@@ -25,26 +60,31 @@ export default function InvitationController(app) {
       );
 
       if ("error" in newInvitation) {
-        return res.status(400).json({ error: newInvitation.error });
+        throw new Error(newInvitation.error);
       }
 
       // Add the invitation to the invited user's groupInvites
-      await addGroupInviteToUser(newInvitation.invitedUser, newInvitation._id);
+      const invite = await addGroupInviteToUser(
+        newInvitation.invitedUser,
+        newInvitation._id
+      );
 
-      if ("error" in newInvitation) {
-        return res.status(400).json({
-          error: `Error adding group invite to user: ${newInvitation.error}`,
-        });
+      if ("error" in invite) {
+        throw new Error(invite.error);
       }
 
-      return res.status(200).json(newInvitation);
+      res.status(200).json(newInvitation);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: `Error creating invitation: ${error}` });
+      res.status(500).send(error.message);
     }
   };
 
+  /**
+   * Retrieves all pending invitations for a user.
+   * @param req - The request object containing the username
+   * @param res - The response object
+   * @returns Either 200 (success) with a JSON response containing the invitations or 500 (error) with an error message
+   */
   const getAllInvitationsByUser = async (req, res) => {
     const { username } = req.params;
 
@@ -52,17 +92,21 @@ export default function InvitationController(app) {
       const invitations = await getAllPendingInvitationsForUser(username);
 
       if ("error" in invitations) {
-        return res.status(400).json({ error: invitations.error });
+        throw new Error(invitations.error);
       }
 
-      return res.status(200).json(invitations);
+      res.status(200).json(invitations);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: `Error fetching invitations: ${error}` });
+      res.status(500).send(error.message);
     }
   };
 
+  /**
+   * Retrieves all pending invitations for a group.
+   * @param req - The request object containing the groupId
+   * @param res - The response object
+   * @returns Either 200 (success) with a JSON response containing the invitations or 500 (error) with an error message
+   */
   const getAllInvitationsByGroup = async (req, res) => {
     const { groupId } = req.params;
 
@@ -70,20 +114,30 @@ export default function InvitationController(app) {
       const invitations = await getAllPendingInvitationsForGroup(groupId);
 
       if ("error" in invitations) {
-        return res.status(400).json({ error: invitations.error });
+        throw new Error(invitations.error);
       }
 
-      return res.status(200).json(invitations);
+      res.status(200).json(invitations);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: `Error retrieving pending invitations: ${error}` });
+      res.status(500).send(error.message);
     }
   };
 
+  /**
+   * Responds to an invitation by updating its status and performing necessary actions based on the status.
+   * @param req - The request object containing the invitationId and status
+   * @param res - The response object
+   * @returns Either 200 (success) with a JSON response containing the updated invitation or 500 (error) with an error message
+   */
   const respondToInvitation = async (req, res) => {
     const { invitationId } = req.params;
     const { status } = req.body;
+
+    // Validate the status
+    if (!isStatusValid(status)) {
+      res.status(400).json("Invalid status");
+      return;
+    }
 
     try {
       const updatedInvitation = await updateInvitationStatus(
@@ -92,7 +146,7 @@ export default function InvitationController(app) {
       );
 
       if ("error" in updatedInvitation) {
-        return res.status(400).json({ error: updatedInvitation.error });
+        throw new Error(updatedInvitation.error);
       }
 
       // Remove the invitation from the invited user's groupInvites
@@ -100,10 +154,9 @@ export default function InvitationController(app) {
         updatedInvitation.invitedUser,
         invitationId
       );
+
       if ("error" in userUpdateResult) {
-        return res.status(400).json({
-          error: `Error removing group invite from user: ${userUpdateResult.error}`,
-        });
+        throw new Error(userUpdateResult.error);
       }
 
       // If the invitation is accepted, add the group to the user's groups
@@ -114,9 +167,7 @@ export default function InvitationController(app) {
         );
 
         if ("error" in groupUpdateResult) {
-          return res.status(400).json({
-            error: `Error adding group to user: ${groupUpdateResult.error}`,
-          });
+          throw new Error(groupUpdateResult.error);
         }
 
         // Add the user to the group's members
@@ -126,9 +177,7 @@ export default function InvitationController(app) {
         );
 
         if ("error" in groupMemberUpdateResult) {
-          return res.status(400).json({
-            error: `Error adding user to group: ${groupMemberUpdateResult.error}`,
-          });
+          throw new Error(groupMemberUpdateResult.error);
         }
 
         // Add the user to the group's queues
@@ -138,9 +187,7 @@ export default function InvitationController(app) {
         );
 
         if ("error" in queueUpdateResult) {
-          return res.status(400).json({
-            error: `Error adding user to group queues: ${queueUpdateResult.error}`,
-          });
+          throw new Error(queueUpdateResult.error);
         }
       }
 
@@ -149,20 +196,22 @@ export default function InvitationController(app) {
         const deletedInvitation = await deleteInvitation(invitationId);
 
         if ("error" in deletedInvitation) {
-          return res.status(400).json({
-            error: `Error deleting invitation: ${deletedInvitation.error}`,
-          });
+          throw new Error(deletedInvitation.error);
         }
       }
 
-      return res.status(200).json(updatedInvitation);
+      res.status(200).json(updatedInvitation);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: `Error updating invitation status: ${error}` });
+      res.status(500).send(error.message);
     }
   };
 
+  /**
+   * Deletes a sent invitation and removes it from the invited user's groupInvites.
+   * @param req - The request object containing the invitationId
+   * @param res - The response object
+   * @returns Either 200 (success) with a JSON response containing the deleted invitation or 500 (error) with an error message
+   */
   const deleteSentInvitation = async (req, res) => {
     const { invitationId } = req.params;
 
@@ -170,7 +219,7 @@ export default function InvitationController(app) {
       const deletedInvitation = await deleteInvitation(invitationId);
 
       if ("error" in deletedInvitation) {
-        return res.status(400).json({ error: deletedInvitation.error });
+        throw new Error(deletedInvitation.error);
       }
 
       // Remove the invitation from the invited user's groupInvites
@@ -178,20 +227,18 @@ export default function InvitationController(app) {
         deletedInvitation.invitedUser,
         invitationId
       );
+
       if ("error" in userUpdateResult) {
-        return res.status(400).json({
-          error: `Error removing group invite from user: ${userUpdateResult.error}`,
-        });
+        throw new Error(userUpdateResult.error);
       }
 
-      return res.status(200).json(deletedInvitation);
+      res.status(200).json(deletedInvitation);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: `Error deleting invitation: ${error}` });
+      res.status(500).send(error.message);
     }
   };
 
+  // Routes for invitation-related operations
   app.post("/api/invitation", sendNewInvitation);
   app.get("/api/invitation/:username", getAllInvitationsByUser);
   app.get("/api/invitation/group/:groupId", getAllInvitationsByGroup);
