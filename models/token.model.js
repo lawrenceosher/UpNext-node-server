26dc7df3c195f14ecf750eae7570dd162638cd4f
Lawrence Osher <lao2205@gmail.com>
2025-05-20 21:34:51 -0400

    deleting mongo approach

--- models/token.model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import tokenSchema from "../schemas/token.schema.js";
-
-const TokenModel = mongoose.model("TokenModel", tokenSchema);
-
-export default TokenModel;
