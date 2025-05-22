97a88a371af95ac668976eef504cab2a090ecfc4
Lawrence Osher <lao2205@gmail.com>
2025-05-22 14:40:31 -0400

    finished tests for creating different queues

--- __tests__/mockData.js
+++ __tests__/mockData.js
@@ -1,5 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const groupId = uuidv4();
 export const movieQueueId = uuidv4();
+export const tvQueueId = uuidv4();
+export const albumQueueId = uuidv4();
+export const bookQueueId = uuidv4();
+export const videoGameQueueId = uuidv4();
+export const podcastQueueId = uuidv4();
 
 export const testUsername = "testUser";
 
@@ -14,5 +21,51 @@ export const mockMovieQueue = {
 };
 
 export const mockTVQueue = {
-    
+  _id: tvQueueId,
+  mediaType: "TV",
+  users: [testUsername],
+  group: groupId,
+  current: [],
+  history: [],
+  media: "TVModel",
+};
+
+export const mockAlbumQueue = {
+  _id: albumQueueId,
+  mediaType: "Album",
+  users: [testUsername],
+  group: groupId,
+  current: [],
+  history: [],
+  media: "AlbumModel",
+};
+
+export const mockBookQueue = {
+  _id: bookQueueId,
+  mediaType: "Book",
+  users: [testUsername],
+  group: groupId,
+  current: [],
+  history: [],
+  media: "BookModel",
+};
+
+export const mockVideoGameQueue = {
+  _id: videoGameQueueId,
+  mediaType: "VideoGame",
+  users: [testUsername],
+  group: groupId,
+  current: [],
+  history: [],
+  media: "VideoGameModel",
+};
+
+export const mockPodcastQueue = {
+  _id: podcastQueueId,
+  mediaType: "Podcast",
+  users: [testUsername],
+  group: groupId,
+  current: [],
+  history: [],
+  media: "PodcastModel",
 };
--- __tests__/services/queueService.spec.js
+++ __tests__/services/queueService.spec.js
@@ -1,5 +1,4 @@
 import * as mockingoose from "mockingoose";
-import { v4 as uuidv4 } from "uuid";
 import QueueModel from "../../models/queue.model.js";
 import MovieModel from "../../models/movie.model.js";
 import TVModel from "../../models/tv.model.js";
@@ -7,12 +6,17 @@ import AlbumModel from "../../models/album.model.js";
 import BookModel from "../../models/book.model.js";
 import VideoGameModel from "../../models/game.model.js";
 import PodcastModel from "../../models/podcast.model.js";
-import { createMovieQueue } from "../../services/internal/queueService.js";
+import { createAlbumQueue, createMovieQueue, createTVQueue, createBookQueue, createPodcastQueue, createVideoGameQueue } from "../../services/internal/queueService.js";
 import {
   mockMovieQueue,
   testUsername,
   groupId,
   movieQueueId,
+  mockTVQueue,
+  mockAlbumQueue,
+  mockVideoGameQueue,
+  mockPodcastQueue,
+  mockBookQueue,
 } from "../mockData.js";
 
 describe("Queue Service", () => {
@@ -62,9 +66,173 @@ describe("Queue Service", () => {
   });
 
   describe("createTVQueue", () => {
-    it("should create a new tv queue with valid inputs", async () => {
+    it("should create a new TV queue with valid inputs", async () => {
       // Arrange
-      mockingoose(QueueModel).toReturn();
+      mockingoose(QueueModel).toReturn(mockTVQueue, "create");
+
+      // Act
+      const result = await createTVQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("_id");
+      expect(result.mediaType).toEqual("TV");
+      expect(result.users).toContain(testUsername);
+      expect(result.group).toEqual(groupId);
+      expect(result.current).toEqual([]);
+      expect(result.history).toEqual([]);
+      expect(result.media).toEqual("TVModel");
+    });
+
+    it("should return an error message when creating a queue fails", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(
+        new Error("Error creating TV queue"),
+        "$save"
+      );
+
+      // Act
+      const result = await createTVQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("error");
+      expect(result.error).toEqual("Error creating TV queue");
+    });
+  });
+
+  describe("createAlbumQueue", () => {
+    it("should create a new album queue with valid inputs", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(mockAlbumQueue, "create");
+
+      // Act
+      const result = await createAlbumQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("_id");
+      expect(result.mediaType).toEqual("Album");
+      expect(result.users).toContain(testUsername);
+      expect(result.group).toEqual(groupId);
+      expect(result.current).toEqual([]);
+      expect(result.history).toEqual([]);
+      expect(result.media).toEqual("AlbumModel");
+    });
+
+    it("should return an error message when creating a queue fails", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(
+        new Error("Error creating album queue"),
+        "$save"
+      );
+
+      // Act
+      const result = await createAlbumQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("error");
+      expect(result.error).toEqual("Error creating album queue");
+    });
+  });
+
+  describe("createBookQueue", () => {
+    it("should create a new book queue with valid inputs", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(mockBookQueue, "create");
+
+      // Act
+      const result = await createBookQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("_id");
+      expect(result.mediaType).toEqual("Book");
+      expect(result.users).toContain(testUsername);
+      expect(result.group).toEqual(groupId);
+      expect(result.current).toEqual([]);
+      expect(result.history).toEqual([]);
+      expect(result.media).toEqual("BookModel");
+    });
+
+    it("should return an error message when creating a queue fails", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(
+        new Error("Error creating book queue"),
+        "$save"
+      );
+
+      // Act
+      const result = await createBookQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("error");
+      expect(result.error).toEqual("Error creating book queue");
+    });
+  });
+
+  describe("createPodcastQueue", () => {
+    it("should create a new podcast queue with valid inputs", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(mockPodcastQueue, "create");
+
+      // Act
+      const result = await createPodcastQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("_id");
+      expect(result.mediaType).toEqual("Podcast");
+      expect(result.users).toContain(testUsername);
+      expect(result.group).toEqual(groupId);
+      expect(result.current).toEqual([]);
+      expect(result.history).toEqual([]);
+      expect(result.media).toEqual("PodcastModel");
+    });
+
+    it("should return an error message when creating a queue fails", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(
+        new Error("Error creating podcast queue"),
+        "$save"
+      );
+
+      // Act
+      const result = await createPodcastQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("error");
+      expect(result.error).toEqual("Error creating podcast queue");
+    });
+  }
+  );
+
+  describe("createVideoGameQueue", () => {
+    it("should create a new video game queue with valid inputs", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(mockVideoGameQueue, "create");
+
+      // Act
+      const result = await createVideoGameQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("_id");
+      expect(result.mediaType).toEqual("VideoGame");
+      expect(result.users).toContain(testUsername);
+      expect(result.group).toEqual(groupId);
+      expect(result.current).toEqual([]);
+      expect(result.history).toEqual([]);
+      expect(result.media).toEqual("VideoGameModel");
+    });
+
+    it("should return an error message when creating a queue fails", async () => {
+      // Arrange
+      mockingoose(QueueModel).toReturn(
+        new Error("Error creating video game queue"),
+        "$save"
+      );
+
+      // Act
+      const result = await createVideoGameQueue(testUsername, groupId);
+
+      // Assert
+      expect(result).toHaveProperty("error");
+      expect(result.error).toEqual("Error creating video game queue");
     });
   });
 });
