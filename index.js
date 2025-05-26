631839ce8d54275e0957087d7c55c8cce41f71ca
Lawrence Osher <lao2205@gmail.com>
2025-05-26 11:34:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const sessionOptions = {
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
-    sameSite: "Lax",
+    sameSite: "none",
     secure: true,
     domain: process.env.NODE_SERVER_DOMAIN,
   };
